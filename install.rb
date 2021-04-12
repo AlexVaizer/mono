afa4c71fc36a228cfe2d8fa78fbc91693b586e8a
Alex Vaizer <wiseralex@gmail.com>
2021-04-12 12:58:54 +0300

    fix of service setup

--- install.rb
+++ install.rb
@@ -10,34 +10,34 @@ puts "Please enter [1-#{ips.count}] number and hit Enter"
 i = gets
 i = i.to_i
 ip = ips[i-1]
-puts "IP address chosen: #{@ip}"
+puts "IP address chosen: #{ip}"
 puts "----------------------------------------------"
 
 # GET PORT
 puts "Please enter PORT number and hit Enter"
 port_s = gets
 port = port_s.to_i
-puts "Port chosen: #{@port}"
+puts "Port chosen: #{port}"
 puts "----------------------------------------------"
 
 # GET MONOBANK TOKEN
 puts "Please enter Monobank Auth Token and hit Enter"
 mono_token = gets.chomp
-puts "Token chosen: #{@mono_token}"
+puts "Token chosen: #{mono_token}"
 puts "----------------------------------------------"
 
 # GET BASIC AUTH SETTINGS
 puts "Please enter Username for Basic Auth"
 mono_user = gets.chomp
-puts "Basic Auth Username chosen: #{@mono_user}"
+puts "Basic Auth Username chosen: #{mono_user}"
 puts "----------------------------------------------"
 
 puts "Please enter Password for Basic Auth"
 mono_pass = gets.chomp
-puts "Basic Auth Password chosen: #{@mono_pass}"
+puts "Basic Auth Password chosen: #{mono_pass}"
 puts "----------------------------------------------"
 
-env_values = "MONO_SERV_IP='#{@ip}' MONO_SERV_PORT='#{@port}' MONO_TOKEN='#{@mono_token}' MONO_BASIC_AUTH_USER='#{@mono_user}' MONO_BASIC_AUTH_PASS='#{@mono_pass}'"
+env_values = "MONO_SERV_IP='#{ip}' MONO_SERV_PORT='#{port}' MONO_TOKEN='#{mono_token}' MONO_BASIC_AUTH_USER='#{mono_user}' MONO_BASIC_AUTH_PASS='#{mono_pass}'"
 puts "(WORKS ONLY IN UBUNTU) Do you want to set up service [y/n]"
 service_setup = gets.chomp
 until ['y','n'].include?(service_setup)

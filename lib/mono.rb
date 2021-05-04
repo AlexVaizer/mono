1046474c5a817eac6292ee2fc55d9af840b8a8c9
AlexVaizer <37403333+AlexVaizer@users.noreply.github.com>
2021-05-05 01:17:06 +0300

    Update mono.rb

--- lib/mono.rb
+++ lib/mono.rb
@@ -3,19 +3,11 @@ class MonobankConnector
 	require 'uri'
 	require 'json'
 	
-	attr_accessor :env, :client_info, :accounts, :statements 
+	attr_accessor :client_info, :accounts, :statements 
 	
-	def initialize(env)
-		@env = env
+	def initialize()
 		@client_info = {}
 		@accounts = []
 		@statements = []
-	end
-	def get_client_info
-		DataFactory.get_client_info(self)
-	end
-
-	def get_statements(account)
-		DataFactory.get_statements(self,account)
 	end
 end
